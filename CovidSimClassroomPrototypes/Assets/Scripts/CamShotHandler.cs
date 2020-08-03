4e7f8ba6e85ec89311b65c22fb8c8168c45e611c
htaylor302 <66102578+htaylor302@users.noreply.github.com>
2020-08-03 14:07:09 -0700

    ACTUAL First Commit
    Please work. Streamlined / sequential version of the 8 decision points.

---
+++ CovidSimClassroomPrototypes/Assets/Scripts/CamShotHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShotHandler : MonoBehaviour
+{
+    #region VARIABLES
+    [SerializeField] private Camera[] covidCams = null;
+    public string characterType;
+    #endregion
+
+
+    #region MONOBEHAVIOUR
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            for (int i = 0; i < covidCams.Length; i++) {
+                covidCams[i].GetComponent<ScreenshotCameraScript>().TakeScreenshot(1920, 1080, i, characterType);
+            }
+        }
+    }
+    #endregion
+}
